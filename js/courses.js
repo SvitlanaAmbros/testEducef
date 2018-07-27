7012adb9a562baa0823f96cd373755072c1ec0ba
SvitlanaAmbros <svitlana.ambros@gmail.com>
2018-07-27 17:48:12 +0300

    create project

---
+++ js/courses.js
@@ -0,0 +1,271 @@
+var coursesApp = [
+		{	icon:"card_img",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img2",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img3",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img4",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img2",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img3",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img4",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img2",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img3",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img4",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img2",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img3",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img4",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img2",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img3",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img4",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img2",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img3",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		},
+		{	icon:"card_img4",
+			name: "Neutal Networks And For Deep Learning",
+			author:{
+				name:"Anthony",
+				surname: "Leath",
+
+			},
+			likes:349,
+			people:2678,
+			price: "$100"
+		}
+
+
+
+];
+
+var menu = ["Home", "Explore", "Course", "Partner", "Event", "Membership", "Forum", "Elements", "Pages"];
